75ac84f2751d559352c972850edd57ca7fe249d5
MarcinKucharsk1 <marcin.kucharski.stud@pw.edu.pl>
2021-01-15 08:35:20 +0000

    zad 3

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -1,5 +1,19 @@
 #pragma once
 
 #include "zad1.hpp"
-
+using namespace std;
 //tutaj szablon ArtystaPlusInstrument
+
+
+template < typename T >
+class ArtystaPlusInstrument
+{
+public:
+    ArtystaPlusInstrument(const Artysta& artysta, const T& instrument) : a(artysta), i(instrument) {}
+
+    unsigned koncert() { return i(a); }
+
+private:
+    Artysta a;
+    T       i;
+};
