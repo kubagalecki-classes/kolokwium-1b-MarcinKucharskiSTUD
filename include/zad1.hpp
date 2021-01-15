5eb3fce9bce343f0255c711a1694f2e556eeeef5
MarcinKucharsk1 <marcin.kucharski.stud@pw.edu.pl>
2021-01-15 07:58:58 +0000

    push

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -13,11 +13,11 @@ class Artysta
 public:
     Artysta() : pseudonim{"Anonim"}, liczba_sluchaczy{0} {};
 
-    Artysta(const string& name, unsigned liczba) : pseudonim(name), liczba_sluchaczy(liczba) {};
-    void graj(ostream& stream) { stream << pseudonim << "   " << liczba_sluchaczy; }
+    Artysta(const string& name, unsigned liczba) : pseudonim{name}, liczba_sluchaczy{liczba} {};
+    void graj(ostream& stream) const { stream << pseudonim << "   " << liczba_sluchaczy; }
 
-    string getPseudonim() { return pseudonim; }
-    unsigned getliczbasluchaczy() { return liczba_sluchaczy; }
+    const string& getPseudonim() const { return pseudonim; }
+    unsigned      getSluchacze() const { return liczba_sluchaczy; }
 
 private:
     string pseudonim;
