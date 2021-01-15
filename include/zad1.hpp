01c8847fb42983b7c25fd85a020db4289d90460e
MarcinKucharsk1 <marcin.kucharski.stud@pw.edu.pl>
2021-01-15 07:48:06 +0000

    zad1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -5,5 +5,21 @@
 #include <ostream>
 #include <sstream>
 #include <string>
+using namespace std;
 
 // tutaj klasa Artysta
+class Artysta
+{
+public:
+    Artysta() : pseudonim{"Anonim"}, liczba_sluchaczy{0} {};
+
+    Artysta(const string& name, unsigned liczba) : pseudonim(name), liczba_sluchaczy(liczba) {};
+    void graj(ostream& stream) { stream << pseudonim << "   " << liczba_sluchaczy; }
+
+    string getPseudonim() { return pseudonim; }
+    unsigned getliczbasluchaczy() { return liczba_sluchaczy; }
+
+private:
+    string pseudonim;
+    unsigned liczba_sluchaczy;
+};
