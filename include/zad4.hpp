6cc1ee1f8687caa053ca310b78b836852a71c9e0
MarcinKucharsk1 <marcin.kucharski.stud@pw.edu.pl>
2021-01-15 08:58:43 +0000

    Nie zdazylem pomyslec nad 4 dobrze wiec zostawiam #

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,5 +3,16 @@
 #include <exception>
 #include <vector>
 using namepsace std;
+
 // tutaj funkcja stworzZespol
-void stworzZespol(){}
+void stworzZespol(int n,const string& a){
+
+    vector< artysta > v;
+    if (a.size() != n)
+        throw logic_error("tres zadania 4"); }
+   // nie zdazylem
+
+}
+
+
+
