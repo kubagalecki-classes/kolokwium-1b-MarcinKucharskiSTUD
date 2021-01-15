e578fb69d11d2b525b8711918e4599ce13bcd56f
MarcinKucharsk1 <marcin.kucharski.stud@pw.edu.pl>
2021-01-15 08:19:10 +0000

    zad2

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -4,4 +4,37 @@
 
 #include <cctype>
 
+
+using namespace std;
 // tutaj klasy PopGwiazda, RapGwiazda oraz funkcja stworzArtyste
+
+class PopGwiazda : public Artysta
+{
+public:
+    PopGwiazda(const string& name, unsigned liczba) : Artysta(name, liczba) {}
+
+    void graj(ostream& stream) const override 
+    { stream << "PopGwiazda: " << getPseudonim(); }
+private:
+
+};
+
+class RapGwiazda : public Artysta
+{
+public:
+
+    RapGwiazda(const string& name, unsigned liczba) : Artysta(name, liczba) {}
+
+    void graj(ostream& stream) const override 
+    { stream << "RapGwiazda: " << getPseudonim(); }
+private:
+}; 
+
+
+Artysta* stworzArtyste(const string& name)
+{
+    if (name.front() == std::toupper(name.front()))
+        return new PopGwiazda{"BTS", 1234};
+    else
+        return new RapGwiazda{"Ye", 4321};
+};
